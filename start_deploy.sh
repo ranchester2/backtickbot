fcd3ef3faaf7f19d061c44161c02b0706fbb1dd6
Sabreh Vardas <sabrehvardas@gmail.com>
2020-10-30 22:31:53 +0200

    use venv and requirements.txt

--- start_deploy.sh
+++ start_deploy.sh
@@ -1,5 +1,9 @@
 while :
 do
+	rm -r backtickbot-venv
+	source backtickbot-venv/bin/activate
+	touch runtime/optout.json
+	touch runtime/responses.json
 	pip3 install -r requirements.txt
 	python3 backtickbot.py
 done
